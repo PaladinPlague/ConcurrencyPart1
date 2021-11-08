62e580db8dd7267c40d3290cb49426fae9370705
Jackson Blair <jackson.blair.2019@uni.strath.ac.uk>
2021-11-08 14:56:36 +0000

    Reorganised account classes to make use of deposit and withdraw methods

--- src/Account.java
+++ src/Account.java
@@ -8,8 +8,6 @@ public abstract class Account {
     private String accountNumber;
     //Money stored within account
     private double balance;
-    //Holds the number of transactions stored in the account;
-    private ArrayList<Transaction> transactions;
 
     /* NOTE FROM JACKSON: As of now, general account details are very basic.
     This works for our project, but if we wanted to add more details to make
@@ -24,8 +22,6 @@ public abstract class Account {
         this.accountNumber = accountNo;
         //Declare opening balance of account from double parameter
         this.balance = openingBalance;
-        //Declare Transactions initially as empty list
-        this.transactions = new ArrayList<>();
     }
 
     //Return the account number of the account
@@ -38,39 +34,22 @@ public abstract class Account {
         return this.balance;
     }
 
-    //Return the list of transactions
-    public ArrayList<Transaction> getTransactions() {
-        return this.transactions;
-    }
-
     //Return the current account type e.g. if savings account, return "Savings Account"
     public abstract String getType();
 
-    //Update the current balance of the class and store the transaction for this
-    //Due to editing information, declare the class as being synchronized
-    //Use protected so that balance cannot be changed directly from other accounts, rather via methods
-    protected synchronized void updateBalance(Transaction transact) {
-        this.balance += transact.getAmount();
-        transactions.add(transact);
-    }
+    //Carry out depositing methods of this account
+    public abstract void deposit(Account sender, Double amount);
 
-    //Deposit a set amount from another account into this account and save it to list of transactions
-    //Due to editing information, declare the class as being synchronized
-    public synchronized void deposit(Double amount, Account sender) {
-        this.balance += amount;
-        transactions.add(new Transaction(amount, sender, this));
-    }
+    //Carry out the withdraw methods of this account
+    public abstract void withdraw(Account receiver, Double amount);
 
-    //Withdraw a set amount from this account into another account and save it to list of transactions
-    //Due to editing information, declare the class as being synchronized
-    public synchronized void withdraw(Double amount, Account receiver) {
-        this.balance -= amount;
-        transactions.add(new Transaction(amount, this, receiver));
-    }
+    //Carryout the transfer methods of this account
+    public abstract void transfer();
 
-    //Change the current balance to a new value
+    //Change the current balance to a new value, ensuring it is at 2 decimal places
     //Due to editing information, declare the class as being synchronized
+    //Methods can't be abstract and synchronized, so you classes must override this method
     public synchronized void setBalance(Double newBalance) {
-        this.balance = newBalance;
+        this.balance = ((double) (Math.round(newBalance * 100))) / 100;
     }
 }
--- src/CurrentAccount.java
+++ src/CurrentAccount.java
@@ -1,25 +1,50 @@
+import java.util.ArrayList;
+
 //Account type that's used for everyday life services
 public class CurrentAccount extends Account{
 
+    //Holds the number of transactions stored in the account;
+    private ArrayList<Transaction> transactions;
     //Identification number of account used to confirm transactions
     private int PIN;
 
     //Initiate current account with all fields filled in
     public CurrentAccount(String accountNo, Double openingBalance, int pin) {
         super(accountNo, openingBalance);
+        //Declare Transactions initially as empty list
+        this.transactions = new ArrayList<>();
         this.PIN = pin;
     }
 
     //Allow validation of a payment from account holder to take place via PIN
     //Due to editing information, declare the class as being synchronized
-    public synchronized boolean confirmPayment(Transaction transaction, int input) {
+    public synchronized boolean confirmPayment(Account target, Double amount, int input) {
         //If input PIN matches account PIN, allow transaction to take place
         if (input == PIN) {
-            this.updateBalance(transaction);
+            this.withdraw(target, amount);
             return true;
         }
         //Otherwise, show this was unsuccessful by returning false
         return false;
+    }
+
+    //Deposit a set amount from another account into this account and save it to list of transactions
+    //Due to editing information, declare the class as being synchronized
+    public synchronized void deposit(Account sender, Double amount) {
+        setBalance(getBalance() + amount);
+        transactions.add(new Transaction(amount, sender, this));
+    }
+
+    //Withdraw a set amount from this account into another account and save it to list of transactions
+    //Due to editing information, declare the class as being synchronized
+    public synchronized void withdraw(Account receiver, Double amount) {
+        setBalance(getBalance() - amount);
+        transactions.add(new Transaction(amount, this, receiver));
+    }
+
+    //Currently unsure how to approach transfer methods for this account
+    public synchronized void transfer() {
+
     }
 
     //Allow user to change PIN if they can successfully enter the current number
@@ -37,5 +62,15 @@ public class CurrentAccount extends Account{
     //Show that the type of the account is a current account
     public String getType() {
         return "Current Account";
+    }
+
+    //Return the list of transactions
+    public ArrayList<Transaction> getTransactions() {
+        return this.transactions;
+    }
+
+    //Return the account's PIN number
+    public int getPIN() {
+        return this.PIN;
     }
 }
