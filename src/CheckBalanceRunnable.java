78789bdce2a983cf8154ec0ecb7a5b9671cfcdf0
Jackson Blair <jackson.blair.2019@uni.strath.ac.uk>
2021-11-13 16:34:25 +0000

    The method of testing concurrency has now been reworked to use a "BankSystem" class that calls the methods for customers in a particular way. Tests for the first two situations have been created and the output is satisfactory.

--- src/CheckBalanceRunnable.java
+++ src/CheckBalanceRunnable.java
@@ -1,19 +1,21 @@
 //An account holder checks the balance of one of their accounts
 public class CheckBalanceRunnable implements Runnable {
     private static final int DELAY = 1;
-    private AccountHolder accHolder;
+    private BankSystem bank;
+    private int holderIndex;
     private int accIndex;
 
-    public CheckBalanceRunnable(AccountHolder holder, int index) {
-        accHolder = holder;
-        accIndex = index;
+    public CheckBalanceRunnable(BankSystem system, int holder, int account) {
+        bank = system;
+        holderIndex = holder;
+        accIndex = account;
     }
 
     //Thread process of this method
     public void run() {
         try
         {
-            accHolder.getBalance(accIndex);
+            bank.checkBalance(holderIndex, accIndex);
             Thread.sleep(DELAY);
         }
         catch (InterruptedException exception) {}
--- src/Driver.java
+++ src/Driver.java
@@ -1,14 +1,25 @@
-/**
- * This program performs the tasks involving thread listed in the specifications
- */
+//This program performs the tasks involving thread listed in the specifications
 public class Driver {
 
     public static void main(String[] args) {
 
-        //Call each of the 6 tests listed
+        //Declare the bank system where all operations are carried out
+        BankSystem bank = new BankSystem();
 
-        simultaneousBalanceCheck();
-        checkAndChangeBalance();
+        //Declare the account holders before adding them to the bank
+        AccountHolder accHolder1 = new AccountHolder("Alice", 20);
+        AccountHolder accHolder2 = new AccountHolder("Bob", 20);
+        bank.addAccountHolder(accHolder1);
+        bank.addAccountHolder(accHolder2);
+
+        //Declare an account that is held by both account holders
+        CurrentAccount acc = new CurrentAccount("11111111", 25.00);
+        accHolder1.addAccount(acc);
+        accHolder2.addAccount(acc);
+
+        //Call each of the 6 tests listed
+        simultaneousBalanceCheck(bank);
+        checkAndChangeBalance(bank);
         //simultaneousChangeAndCheck();
         //changeCheckAndTransfer();
         //insufficientFunds();
@@ -16,55 +27,43 @@ public class Driver {
     }
 
     //Two account holders are trying to check the balance simultaneously
-    public static void simultaneousBalanceCheck() {
+    public static void simultaneousBalanceCheck(BankSystem bank) {
 
-        //Declare the account holders
-        AccountHolder accHolder1 = new AccountHolder("Alice", 20);
-        AccountHolder accHolder2 = new AccountHolder("Bob", 20);
-
-        //Declare an account that is held by both account holders
-        CurrentAccount acc = new CurrentAccount("11111111", 25.00);
-        accHolder1.addAccount(acc);
-        accHolder2.addAccount(acc);
 
         //Set up the Runnable case for both account holders on the only account in their list of accounts
-        CheckBalanceRunnable check1 = new CheckBalanceRunnable(accHolder1, 0);
-        CheckBalanceRunnable check2 = new CheckBalanceRunnable(accHolder2, 0);
+        CheckBalanceRunnable check1 = new CheckBalanceRunnable(bank, 0, 0);
+        CheckBalanceRunnable check2 = new CheckBalanceRunnable(bank, 1, 0);
 
         //Declare threads for the runnable cases
         Thread accH1T = new Thread(check1);
         Thread accH2T = new Thread(check2);
 
-        //Start the process of the threads
-        accH1T.start();
-        accH2T.start();
+        //Use synchronized block so that the order of these threads are isolated from threads in other methods
+        synchronized (bank) {
+            //Start the process of the threads
+            accH1T.start();
+            accH2T.start();
+        }
 
     }
 
-    //One account holder is trying to check the balance while the other is depositing/withdrawing money.
-    public static void checkAndChangeBalance() {
+    public static void checkAndChangeBalance(BankSystem bank) {
 
-        //Declare the account holders
-        AccountHolder accHolder1 = new AccountHolder("Charlie", 20);
-        AccountHolder accHolder2 = new AccountHolder("David", 20);
-
-        //Declare an account that is held by both account holders
-        CurrentAccount acc = new CurrentAccount("22222222", 25.00);
-        accHolder1.addAccount(acc);
-        accHolder2.addAccount(acc);
-
-        //Set up the Runnable case for first bank holder checking the account balance
-        CheckBalanceRunnable check1 = new CheckBalanceRunnable(accHolder1, 0);
-        //Set up the Runnable case for second bank holder depositing money into bank
-        DepositRunnable deposit2 = new DepositRunnable(accHolder2, 0, 10.00);
+        //Set up the Runnable case for both account holders on the only account in their list of accounts
+        CheckBalanceRunnable check1 = new CheckBalanceRunnable(bank, 0, 0);
+        DepositWithdrawRunnable deposit2 = new DepositWithdrawRunnable(bank, 1, 0, 10.00, false);
 
         //Declare threads for the runnable cases
         Thread accH1T = new Thread(check1);
         Thread accH2D = new Thread(deposit2);
 
-        //Start the process of the threads
-        accH2D.start();
-        accH1T.start();
+        //Use synchronized block so that the order of these threads are isolated from threads in other methods
+        synchronized (bank) {
+            //Start the process of the threads
+            accH1T.start();
+            accH2D.start();
+        }
+
     }
 
     //The two account holders are trying simultaneously to deposit/withdraw money & check the balance.
