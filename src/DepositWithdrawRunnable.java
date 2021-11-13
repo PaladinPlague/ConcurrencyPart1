4a7050a43382d1147662e8918b3076eff8e2a431
Jackson Blair <jackson.blair.2019@uni.strath.ac.uk>
2021-11-13 16:39:13 +0000

    The method of testing concurrency has now been reworked to use a "BankSystem" class that calls the methods for customers in a particular way. Tests for the first two situations have been created and the output is satisfactory.

---
+++ src/DepositWithdrawRunnable.java
@@ -0,0 +1,33 @@
+//An account holder can either deposit or withdraw an amount from a thread
+public class DepositWithdrawRunnable implements Runnable {
+    private static final int DELAY = 1;
+    private BankSystem bank;
+    private int holderIndex;
+    private int accIndex;
+    private double amount;
+    private boolean isWithdraw;
+
+    public DepositWithdrawRunnable(BankSystem system, int holder, int account, double depoAmount, boolean withdraw) {
+        bank = system;
+        holderIndex = holder;
+        accIndex = account;
+        amount = depoAmount;
+        isWithdraw = withdraw;
+    }
+
+    //Thread process of this method - depending on value of boolean parameter, process is deposit (if false) or withdraw (if true)
+    public void run() {
+        try
+        {
+            if (isWithdraw) {
+                bank.withdraw(holderIndex, accIndex, amount);
+            } else {
+                bank.deposit(holderIndex, accIndex, amount);
+            }
+            Thread.sleep(DELAY);
+        }
+        catch (InterruptedException exception) {
+
+        } catch (Exception e) {}
+    }
+}
