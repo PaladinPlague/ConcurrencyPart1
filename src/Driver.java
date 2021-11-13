11a1d7da58cef850170a55ef0e271c9c71669bb5
Jackson Blair <jackson.blair.2019@uni.strath.ac.uk>
2021-11-13 13:00:09 +0000

    Adding situation for second testing of documentation. Code has been added to the Account class due to the nature of how Threads work. The output is VERY faulty, and I am having great trouble trying to get all threads to execute in random order, and all synchronized processes carrying out their functions at the same time and not having some functions interweaving in and out of each other.

--- src/Driver.java
+++ src/Driver.java
@@ -4,20 +4,23 @@
 public class Driver {
 
     public static void main(String[] args) {
+
         //Call each of the 6 tests listed
+
         simultaneousBalanceCheck();
         checkAndChangeBalance();
-        simultaneousChangeAndCheck();
-        changeCheckAndTransfer();
-        insufficientFunds();
-        simultaneousModification();
+        //simultaneousChangeAndCheck();
+        //changeCheckAndTransfer();
+        //insufficientFunds();
+        //simultaneousModification();
     }
 
     //Two account holders are trying to check the balance simultaneously
     public static void simultaneousBalanceCheck() {
+
         //Declare the account holders
         AccountHolder accHolder1 = new AccountHolder("Alice", 20);
-        AccountHolder accHolder2 = new AccountHolder("Bob", 22);
+        AccountHolder accHolder2 = new AccountHolder("Bob", 20);
 
         //Declare an account that is held by both account holders
         CurrentAccount acc = new CurrentAccount("11111111", 25.00);
@@ -35,11 +38,33 @@ public class Driver {
         //Start the process of the threads
         accH1T.start();
         accH2T.start();
+
     }
 
     //One account holder is trying to check the balance while the other is depositing/withdrawing money.
     public static void checkAndChangeBalance() {
 
+        //Declare the account holders
+        AccountHolder accHolder1 = new AccountHolder("Charlie", 20);
+        AccountHolder accHolder2 = new AccountHolder("David", 20);
+
+        //Declare an account that is held by both account holders
+        CurrentAccount acc = new CurrentAccount("22222222", 25.00);
+        accHolder1.addAccount(acc);
+        accHolder2.addAccount(acc);
+
+        //Set up the Runnable case for first bank holder checking the account balance
+        CheckBalanceRunnable check1 = new CheckBalanceRunnable(accHolder1, 0);
+        //Set up the Runnable case for second bank holder depositing money into bank
+        DepositRunnable deposit2 = new DepositRunnable(accHolder2, 0, 10.00);
+
+        //Declare threads for the runnable cases
+        Thread accH1T = new Thread(check1);
+        Thread accH2D = new Thread(deposit2);
+
+        //Start the process of the threads
+        accH2D.start();
+        accH1T.start();
     }
 
     //The two account holders are trying simultaneously to deposit/withdraw money & check the balance.
